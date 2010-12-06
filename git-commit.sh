733d00960d7135024ecb0d0e4d01173fedbe0ccf
Louis Lynch <louis.lynch@gmail.com>
2010-12-06 15:37:40 -0500

    FIX support spaces in file name and the add work properly cleanup with the tmp file description of the wierd commands added a mechanism to enable debug mode

--- git-commit.sh
+++ git-commit.sh
@@ -7,27 +7,36 @@
 # filling a file with repo status and change since last commit
 scm=git
 # generate the diff
-tmpfile_diff=$(mktemp -u "/tmp/commit-diff.XXXXXXXXXXXXXXX")
+tmpfile_diff=$(mktemp)
 $scm diff > $tmpfile_diff
 
 # edit the commit message
 # better keep it localy to reuse it if commit fail
-#tmpfile_commit_message=$(mktemp -u "/tmp/commit-message.XXXXXXXXXXXXXXX")
 tmpfile_commit_message=.last-commit~
 
 # generate the status
-tmpfile_commit_status=$(mktemp -u "/tmp/commit-status.XXXXXXXXXXXXXXX")
+tmpfile_commit_status=$(mktemp)
 $scm status >> $tmpfile_commit_status
 
 parse_status() {
     status_file=$1
 
 
+    # Explanations for the expressions above
+    # line 1: cat the content of the status file
+    # line 2: replace + with + newfile:
+    # line 3: select modified:, new file:, deleted:, renamed:
+    # line 4: then handle rename them
+    # line 5: match any [#+].*:(.*) to select name of the file
+    # line 6: escape spaces
+    # line 7: inline everything
     cat $status_file | \
+        sed 's#\+\s*\(.*\)$#+ new file:\1#g;' | \
         grep -E "+modified:|+new file:|+deleted:|+renamed:|\+:.*" | \
         sed 's#\(.*\)->\(.*\)#\# renamed:\1\n\# renamed:\2#g' | \
-        sed 's#\#[^:]*:\s*\(.*\)$#\1#g' | \
-        sed -n '1,$H; $x; $s/\n/ /gp' # inline
+        sed 's#[\#\+][^:]*:\s*\(.*\)$#\1#g' | \
+        sed 's# #\\ #g' | \
+        sed -n '1,$H; $x; $s/\n/ /gp' 
 }
 
 main() {
@@ -38,22 +47,21 @@ vim -c "e $tmpfile_diff" -c vs -c "e $tmpfile_commit_status" -c 0 -c sp -c "e $t
 # after edition we check if we have a commit message file
 if [ -f $tmpfile_commit_message ] 
 then
+  debug=false
   # to debug turn scm to echo
-  #scm=echo
+  $debug && scm="echo"
+  $debug && echo 
 
-  # here we add all the files marqued as + instead of ?
+  # here we add all the files marqued as + instead of #
   cat $tmpfile_commit_status | \
       grep -E "\+\s*+.*" | \
       sed 's#\S\s*\(.*\)$#\1#g;' | \
       xargs -I'{}' $scm add '{}'
 
-  # now we launch the commit operation for every file marqued with 
-  # D, A, M or +, then if the commit is successful we delete the 
-  # message file
-  #parse_status $tmpfile_commit_status
-  $scm commit -F $tmpfile_commit_message \
-      $( parse_status $tmpfile_commit_status ) \
-       && rm $tmpfile_commit_message
+  $debug && parse_status $tmpfile_commit_status
+  # now we launch the commit operation
+  eval "$scm commit -F $tmpfile_commit_message $( parse_status $tmpfile_commit_status )" \
+       && [ $debug != "true" ] && rm $tmpfile_commit_message
 else
   # abord commit
   echo "Commit aborded - The commit message file has not been saved"
